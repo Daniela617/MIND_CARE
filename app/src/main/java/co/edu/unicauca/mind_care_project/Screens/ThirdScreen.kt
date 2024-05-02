968e7adc462356af1adf8d85acd957f187bef2ae
IsabellaSolarte <isabsolarte@unicauca.edu.co>
2024-05-01 20:44:55 -0500

    Se agregó lista de preferencias seleccionadas

--- app/src/main/java/co/edu/unicauca/mind_care_project/Screens/ThirdScreen.kt
+++ app/src/main/java/co/edu/unicauca/mind_care_project/Screens/ThirdScreen.kt
@@ -1,25 +1,18 @@
 package co.edu.unicauca.mind_care_project
 import androidx.compose.foundation.background
-
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-
 import androidx.compose.foundation.layout.Column
-
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -33,7 +26,6 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -42,7 +34,7 @@ fun thirdScreen(onClick: (String) -> Unit) {
     val gradiente = Brush.verticalGradient(
         colors = listOf(Color(0xFF8E66B8), Color(0xFF0B5884))
     )
-    val selectedItems = remember { mutableStateListOf<String>() }
+    val selectedItems = remember { mutableStateListOf<Item>() }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -79,7 +71,17 @@ fun thirdScreen(onClick: (String) -> Unit) {
                 items(listaDeItems) { item ->
                     Box(
                         modifier = Modifier
-                            .background(Color.White, RoundedCornerShape(8.dp))
+                            .clickable(onClick= {
+                                if (selectedItems.contains(item)) {
+                                    selectedItems.remove(item)
+                                } else {
+                                    selectedItems.add(item)
+                                }
+                            })
+                            .background(
+                                if (selectedItems.contains(item)) Color(0xFFCEC2FD) else Color.White,
+                                RoundedCornerShape(8.dp)
+                            )
                             .padding(horizontal = 24.dp, vertical = 32.dp)
 
                     ) {
@@ -123,7 +125,6 @@ fun thirdScreen(onClick: (String) -> Unit) {
 
 
     }
-
 }
 
 data class Item(val nombre: String)
